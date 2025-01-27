bea364fffbad96f1c7bc9a6a07aac962982b46de
BitMaster13 <marcelsachse@gmail.com>
2025-01-27 07:35:59 +0100

    reorgenized scripts

--- Assets/Code/PachinkoBoard/BattleBox.cs
+++ Assets/Code/PachinkoBoard/BattleBox.cs
@@ -2,18 +2,24 @@ using UnityEngine;
 
 public class BattleBox : MonoBehaviour
 {
+    private PachinkoGameManager pachinkoGameManager;
+
+    private void Start()
+    {
+        pachinkoGameManager = FindFirstObjectByType<PachinkoGameManager>();
+        if (pachinkoGameManager == null)
+        {
+            Debug.LogError("PachinkoGameManager not found in the scene", this);
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         Debug.LogError("Pachinko Ball OnTriggerEnter2D");
 
         if (other.gameObject.CompareTag("PachinkoBall"))
         {
-            DestroyPachinkoBall(other.gameObject);
+            pachinkoGameManager.enteredBattleBox(other.gameObject);
         }
-    }
-
-    public void DestroyPachinkoBall(GameObject ball)
-    {
-        Destroy(ball);
     }
 }
--- Assets/Code/PachinkoBoard/PachinkoGameManager.cs
+++ Assets/Code/PachinkoBoard/PachinkoGameManager.cs
@@ -1,6 +1,15 @@
+using System;
 using UnityEngine;
 
 public class PachinkoGameManager : MonoBehaviour
 {
-   
+    internal void enteredBattleBox(GameObject gameObject)
+    {
+        DestroyPachinkoBall(gameObject);
+    }
+
+    public void DestroyPachinkoBall(GameObject ball)
+    {
+        Destroy(ball);
+    }
 }
